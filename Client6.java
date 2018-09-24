273a3661c9551c747134244792657fd667b85296
kumar1118 <43104157+kumar1118@users.noreply.github.com>
2018-09-24 22:39:10 +0530

    Update Client6.java

--- Client6.java
+++ Client6.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.nio.Buffer;
 import java.sql.Statement;
-
 public class Client6 {
 
 	public static void main(String[] args) throws Exception
